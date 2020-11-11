8b44bbc98c2168838aa9422d723b435f6bcad58e
chrismatgit <christianmatabaro@gmail.com>
2020-11-11 16:56:53 +0300

    Postgresql with sequilize for a graphql server Apollo server authentication

--- src/index.js
+++ src/index.js
@@ -1,29 +1,104 @@
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import cors from "cors";
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, AuthenticationError } from "apollo-server-express";
 
 import schema from "./schema";
 import resolvers from "./resolvers";
-import models from "./models";
+import models, { sequelize } from "./models";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 
+const getMe = async (req) => {
+  const token = req.headers["x-token"];
+
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (e) {
+      throw new AuthenticationError("Your session expired. Sign in again.");
+    }
+  }
+};
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
-    models,
-    me: models.users[1],
+  formatError: (error) => {
+    // remove the internal sequelize error message
+    // leave only the important validation error
+    const message = error.message
+      .replace("SequelizeValidationError: ", "")
+      .replace("Validation error: ", "");
+
+    return {
+      ...error,
+      message,
+    };
+  },
+  context: async ({ req }) => {
+    const me = await getMe(req);
+
+    return {
+      models,
+      me,
+      secret: process.env.SECRET,
+    };
   },
 });
 
 const { GRAPHQL_URL } = process.env;
+
 server.applyMiddleware({ app, path: GRAPHQL_URL });
 
-app.listen({ port: process.env.PORT }, () => {
-  console.log("Apollo Server on http://localhost:8000/graphql");
+const eraseDatabaseOnSync = true;
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+  app.listen({ port: process.env.PORT }, () => {
+    console.log("Apollo Server on http://localhost:8000/graphql");
+  });
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: "rwieruch",
+      email: "hello@robin.com",
+      password: "rwieruch",
+      role: 'ADMIN',
+      messages: [
+        {
+          text: "Published the Road to learn React",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+
+  await models.User.create(
+    {
+      username: "ddavids",
+      email: "hello@david.com",
+      password: "ddavids",
+      messages: [
+        {
+          text: "Happy to release ...",
+        },
+        {
+          text: "Published a complete ...",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+};
