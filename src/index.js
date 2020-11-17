4cb6e8251a4ccb92ee118aa89e9a6b4c0afda6d1
chrismatgit <christianmatabaro@gmail.com>
2020-11-17 07:39:30 +0300

    GraphQL Custom Scalars in Apollo Server Pagination in GraphQL with Apollo Server GraphQL Subscriptions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import cors from "cors";
 import express from "express";
+import http from "http";
 import { ApolloServer, AuthenticationError } from "apollo-server-express";
 
 import schema from "./schema";
@@ -9,7 +10,9 @@ import resolvers from "./resolvers";
 import models, { sequelize } from "./models";
 
 dotenv.config();
+
 const app = express();
+
 app.use(cors());
 
 const getMe = async (req) => {
@@ -39,14 +42,21 @@ const server = new ApolloServer({
       message,
     };
   },
-  context: async ({ req }) => {
-    const me = await getMe(req);
+  context: async ({ req, connection }) => {
+    if (connection) {
+      return {
+        models,
+      };
+    }
+    if (req) {
+      const me = await getMe(req);
 
-    return {
-      models,
-      me,
-      secret: process.env.SECRET,
-    };
+      return {
+        models,
+        me,
+        secret: process.env.SECRET,
+      };
+    }
   },
 });
 
@@ -54,27 +64,31 @@ const { GRAPHQL_URL } = process.env;
 
 server.applyMiddleware({ app, path: GRAPHQL_URL });
 
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
 const eraseDatabaseOnSync = true;
 
 sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
   if (eraseDatabaseOnSync) {
-    createUsersWithMessages();
+    createUsersWithMessages(new Date());
   }
-  app.listen({ port: process.env.PORT }, () => {
+  httpServer.listen({ port: process.env.PORT }, () => {
     console.log("Apollo Server on http://localhost:8000/graphql");
   });
 });
 
-const createUsersWithMessages = async () => {
+const createUsersWithMessages = async (date) => {
   await models.User.create(
     {
       username: "rwieruch",
       email: "hello@robin.com",
       password: "rwieruch",
-      role: 'ADMIN',
+      role: "ADMIN",
       messages: [
         {
           text: "Published the Road to learn React",
+          createdAt: date.setSeconds(date.getSeconds() + 1),
         },
       ],
     },
@@ -91,9 +105,11 @@ const createUsersWithMessages = async () => {
       messages: [
         {
           text: "Happy to release ...",
+          createdAt: date.setSeconds(date.getSeconds() + 1),
         },
         {
           text: "Published a complete ...",
+          createdAt: date.setSeconds(date.getSeconds() + 1),
         },
       ],
     },
